c52d1ecbf7b847e9802fd07a899b110d600cfe35
Luis Miguel Morillas <morillas@gmail.com>
2016-05-05 13:47:45 +0200

    updated new schema

--- settings.py
+++ settings.py
@@ -1,106 +1,29 @@
-# Let's just use the local mongod instance. Edit as needed.
+# coding: utf-8
 
-# Please note that MONGO_HOST and MONGO_PORT could very well be left
-# out as they already default to a bare bones local 'mongod' instance.
-#
+MONGO_HOST = 'localhost'
+MONGO_PORT = 27017
+MONGO_DBNAME = 'arasaactest'
 
-import os
-
-MONGO_HOST = os.environ.get('MONGO_HOST', 'localhost')
-MONGO_PORT = os.environ.get('MONGO_PORT', 27017)
-# MONGO_USERNAME = os.environ.get('MONGO_USERNAME', 'user')
-# MONGO_PASSWORD = os.environ.get('MONGO_PASSWORD', 'user')
-MONGO_DBNAME = os.environ.get('MONGO_DBNAME', 'arasaac')
-
-
+# Enable reads (GET), inserts (POST) and DELETE for resources/collections
+# (if you omit this line, the API will default to ['GET'] and provide
+# read-only access to the endpoint).
+# RESOURCE_METHODS = ['GET', 'POST', 'DELETE']
 RESOURCE_METHODS = ['GET']
+
+# Enable reads (GET), edits (PATCH), replacements (PUT) and deletes of
+# individual items  (defaults to read-only item access).
+#ITEM_METHODS = ['GET', 'PATCH', 'PUT', 'DELETE']
 ITEM_METHODS = ['GET']
 
-schema_image = {
-    # (https://github.com/nicolaiarocci/cerberus) for details.
-    'name': {
-        'type': 'string',
-        'minlength': 1,
-        'maxlength': 250,
-        'required': True,
-    },
-    # 'role' is a list, and can only contain values from 'allowed'.
-
-}
-
+from schema import schema_images
 
 images = {
-    # 'title' tag used in item links.
     'item_title': 'image',
-
-    # by default the standard item entry point is defined as
-    # '/people/<ObjectId>/'. We leave it untouched, and we also enable an
-    # additional read-only entry point. This way consumers can also perform GET
-    # requests at '/people/<lastname>/'.
-    'additional_lookup': {
-        'url': 'regex("[\w]+")',
-        'field': 'path'
-    },
-
-    # Schema definition, based on Cerberus grammar. Check the Cerberus project
-    # (https://github.com/nicolaiarocci/cerberus) for details.
-    'schema': {
-        'path': {
-            'type': 'string',
-            'minlength': 1,
-            'maxlength': 250,
-            'required': True,
-            'unique': True,
-         },
-        'license': {
-            'type': 'list',
-            'required': True,
-        },
-        # 'role' is a list, and can only contain values from 'allowed'.
-        'type': {
-            'type': 'objectid',
-        }
-        },
-    # We choose to override global cache-control directives for this resource.
     'cache_control': 'max-age=10,must-revalidate',
     'cache_expires': 10,
-
-    # most global settings can be overridden at resource level
-    'resource_methods': ['GET', 'POST'],
-}
-
-catalog = {
-    # 'title' tag used in item links.
-    'item_title': 'catalog',
-
-    # by default the standard item entry point is defined as
-    # '/people/<ObjectId>/'. We leave it untouched, and we also enable an
-    # additional read-only entry point. This way consumers can also perform GET
-    # requests at '/people/<lastname>/'.
-
-    # Schema definition, based on Cerberus grammar. Check the Cerberus project
-    # (https://github.com/nicolaiarocci/cerberus) for details.
-    'schema': {
-        'label': {
-            'type': 'dict',
-            'required': True,
-         },
-        'subtopicof': {
-            'type': 'objectid',
-        },
-        },
-    # We choose to override global cache-control directives for this resource.
-    'cache_control': 'max-age=10,must-revalidate',
-    'cache_expires': 10,
-
-    # most global settings can be overridden at resource level
-    'resource_methods': ['GET', 'POST'],
+    'resource_methods': ['GET'],
+    'schema': schema_images
 }
 
 
-
-DOMAIN = {
-    'images': images,
-    'catalog':  catalog,
-
-}
+DOMAIN = {'images': images, 'materials': {}, 'lse': {}}
