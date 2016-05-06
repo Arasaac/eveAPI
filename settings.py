c020a6d961a2c5f87f734687f94eb3667aa26cef
Luis Miguel Morillas <morillas@gmail.com>
2016-05-06 11:47:27 +0200

    Add tags

--- settings.py
+++ settings.py
@@ -2,7 +2,9 @@
 
 MONGO_HOST = 'localhost'
 MONGO_PORT = 27017
-MONGO_DBNAME = 'arasaactest'
+# MONGO_USERNAME = 'user'
+#MONGO_PASSWORD = 'user'
+MONGO_DBNAME = 'arasaac'
 
 # Enable reads (GET), inserts (POST) and DELETE for resources/collections
 # (if you omit this line, the API will default to ['GET'] and provide
@@ -18,10 +20,26 @@ ITEM_METHODS = ['GET']
 from schema import schema_images
 
 images = {
+    # 'title' tag used in item links. Defaults to the resource title minus
+    # the final, plural 's' (works fine in most cases but not for 'people')
     'item_title': 'image',
+
+    # by default the standard item entry point is defined as
+    # '/people/<ObjectId>'. We leave it untouched, and we also enable an
+    # additional read-only entry point. This way consumers can also perform
+    # GET requests at '/people/<lastname>'.
+    #'additional_lookup': {
+    #    'url': 'regex("[\w]+")',
+    #    'field': 'lastname'
+    #},
+
+    # We choose to override global cache-control directives for this resource.
     'cache_control': 'max-age=10,must-revalidate',
     'cache_expires': 10,
+
+    # most global settings can be overridden at resource level
     'resource_methods': ['GET'],
+
     'schema': schema_images
 }
 
