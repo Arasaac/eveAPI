8438ec682627ff9eec63df04d8adf8a6f6170224
Luis Miguel Morillas <morillas@gmail.com>
2016-11-09 12:17:15 +0100

    new schemas

---
+++ app/schemas.py
@@ -0,0 +1,73 @@
+schema_images = {
+    'schema': {
+        'creationDate': {
+            'type': 'string'
+        },
+        'modificationDate': {
+            'type': 'string'
+        },
+        'url': {
+            'type': 'string'
+        },
+        'author': {
+            'type': 'objectid',
+            'data_relation': {
+                'resource': 'authors',
+                'field': '_id',
+                'embeddable': True
+            }
+        },
+        'counter': {
+            'type': 'integer'
+        },
+        'license': {
+            'type': 'string'
+        },
+        'type': {
+            'type': 'string'
+        }
+    }
+}
+
+schema_authors = {
+    'schema': {
+        'name': {
+            'type': 'string'
+        },
+        'email': {
+            'type': 'string'
+        }
+    }
+}
+
+schema_words = {
+    'schema': {
+        'word': {
+            'type': 'string'
+        },
+        'lang': {
+            'type': 'string'
+        },
+        'images': {
+            'type': 'list',
+            'schema': {
+                'type': 'objectid',
+                'data_relation': {
+                    'resource': 'images',
+                    'field': '_id',
+                    'embeddable': True
+                }
+            }
+        },
+        'type': {
+            'type': 'string'
+        },
+        'categories': {
+            'type': 'list',
+            'schema': {
+                'type': 'string'
+            }
+        }
+    }
+}
+
